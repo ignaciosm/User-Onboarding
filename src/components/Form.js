73dd56b87e664f41c456950a5584eef003858a40
ignaciosm <ignaciosm@gmail.com>
2019-10-17 17:26:58 -0500

    step 2

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,30 +1,59 @@
 import React from 'react';
-import { Form, Field, Formik } from "formik";
+import { Form, Field, withFormik } from "formik";
+import * as Yup from "yup";
 
 import '../App.css';
 
-const LoginForm = () => {
+function LoginForm({ values, errors, touched }) {
+
   return (
-    <Formik
-        initialValues={{ name: "", email: "", password: "" }}
-        onSubmit={(values, actions) => {
-          console.log(values, actions);
-          actions.resetForm();
-        }}
-        render={props => {
-          return (
-            <Form>
-              <Field name='name' type='text' placeholder='Name' />
-              <Field name='email' type='email' placeholder='Email' />
-              <Field name='password' type='password' placeholder='Password' />
-              <label>Terms of Service<Field name='terms' type='checkbox'/></label>
+    <Form>
+      <div>
+        {touched.name && errors.name && <p>{errors.name}</p>}
+        <Field name='name' type='text' placeholder='Name' />
+      </div>
+      <div>
+        {touched.email && errors.email && <p>{errors.email}</p>}
+        <Field name='email' type='email' placeholder='Email' />
+      </div>
+      <div>
+        {touched.password && errors.password && <p>{errors.password}</p>}
+        <Field name='password' type='password' placeholder='Password' />
+      </div>
+      <label>Terms of Service<Field name='terms' type='checkbox' checked={values.terms} /></label>
 
-              <input type='submit' />
-            </Form>
-          )
-        }}
-    />
+      <input type='submit' />
+    </Form>
   );
-};
+}
 
-export default LoginForm;
+const FormikLoginForm = withFormik({
+  mapPropsToValues({ name, email, password, terms }) {
+    return {
+      name: name || "",
+      email: email || "",
+      password: password || "",
+      terms: terms || false,
+    };
+  },
+  //======VALIDATION SCHEMA==========
+  validationSchema: Yup.object().shape({
+    name: Yup.string().required("Name not valid"),
+    email: Yup.string()
+      .email("Email not valid")
+      .required("Email is required"),
+    password: Yup.string()
+      .min(6, "Password must be 6 characters or longer")
+      .required("Password is required"),
+    // terms: Yup.bool(true),
+  }),
+  //======END VALIDATION SCHEMA==========
+
+  handleSubmit(values, { resetForm } ) {
+    console.log(values);
+    resetForm();
+    //THIS IS WHERE YOU DO YOUR FORM SUBMISSION CODE... HTTP REQUESTS, ETC.
+  }
+})(LoginForm);
+
+export default FormikLoginForm;
