03bfa2c16cfc8b6e3966fefa163381598363eb36
ignaciosm <ignaciosm@gmail.com>
2019-10-17 18:01:04 -0500

    mvp

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Form, Field, withFormik } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
 import '../App.css';
 
-function LoginForm({ values, errors, touched }) {
 
+
+function LoginForm({ values, errors, touched, status }) {
+
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    status && setUsers(users => [...users, status]);
+  }, [status]);
+
+  console.log('users', users)
   return (
+    <div>
     <Form>
       <div>
         {touched.name && errors.name && <p>{errors.name}</p>}
@@ -24,7 +33,16 @@ function LoginForm({ values, errors, touched }) {
       <label>Terms of Service<Field name='terms' type='checkbox' checked={values.terms} /></label>
 
       <input type='submit' />
+
     </Form>
+    {users.map(user => (
+        <ul key={user.id}>
+          <li>Name: {user.name}</li>
+          <li>Email: {user.email}</li>
+          {/* <li>Food: {user.food}</li> */}
+        </ul>
+      ))}
+    </div>
   );
 }
 
@@ -50,12 +68,13 @@ const FormikLoginForm = withFormik({
   }),
   //======END VALIDATION SCHEMA==========
 
-  handleSubmit(values, { resetForm } ) {
+  handleSubmit(values, { setStatus, resetForm } ) {
     console.log(values);
     axios
       .post("https://reqres.in/api/users", values)
       .then(res => {
         console.log(res); // Data was created successfully and logs to console
+        setStatus(res.data);
         resetForm();
       })
       .catch(err => {
