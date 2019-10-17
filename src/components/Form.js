1746f2a22fd85b304fd5f12319682680913cac60
ignaciosm <ignaciosm@gmail.com>
2019-10-17 15:19:13 -0500

    step 1

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import { Form, Field, Formik } from "formik";
+
 import '../App.css';
 
-const Form = () => {
+const LoginForm = () => {
   return (
-    <div>
-      <p className="">Ignacio test</p>
-    </div>
-  );
-}
+    <Formik
+        initialValues={{ name: "", email: "", password: "" }}
+        onSubmit={(values, actions) => {
+          console.log(values, actions);
+          actions.resetForm();
+        }}
+        render={props => {
+          return (
+            <Form>
+              <Field name='name' type='text' placeholder='Name' />
+              <Field name='email' type='email' placeholder='Email' />
+              <Field name='password' type='password' placeholder='Password' />
+              <label>Terms of Service<Field name='terms' type='checkbox'/></label>
 
-export default Form;
+              <input type='submit' />
+            </Form>
+          )
+        }}
+    />
+  );
+};
+
+export default LoginForm;
